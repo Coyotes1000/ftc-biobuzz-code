0c58f06004d344ffa11bd1d244507daab968323d
Alex <alexrichardpoirier@gmail.com>
2026-08-27 13:55:15 -0400

    Creates Command class for action management.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Command.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Command.java
@@ -1,5 +1,43 @@
 package org.firstinspires.ftc.teamcode.framework;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Command {
+
+    public List<Subsystem> requirements = new ArrayList<>();
+
+    public Integer priority = 0;
+
+    public enum State {
+        PENDING,
+        RUNNING,
+        FINISHED
+    }
+
+    public State state = State.PENDING;
+
+    public Command () {
+
+    }
+
+    private void setRequirements (Subsystem... subsystems) {
+        requirements.clear();
+        Collections.addAll(requirements, subsystems);
+    }
+
+    public void start () {
+
+    }
+
+    public void update () {
+
+    }
+
+    public void end (Boolean interrupted) {
+
+    }
     
 }
