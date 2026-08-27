2f89d8a0c2af01081a6e66225909883e9281088b
Alex <alexrichardpoirier@gmail.com>
2026-08-27 13:08:50 -0400

    Creates Scheduler class for Command scheduling and  management.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Scheduler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Scheduler.java
@@ -1,5 +1,105 @@
 package org.firstinspires.ftc.teamcode.framework;
 
-public class Scheduler {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public final class Scheduler {
+
+    private static volatile Scheduler instance = null;
+
+    private final Map<Subsystem, Integer> priorityMap = new HashMap<>();
+
+    private final List<Command> selectedCommands = new ArrayList<>();
+    private final List<Command> pendingCommands = new ArrayList<>();
+    private final List<Command> rejectedCommands = new ArrayList<>();
+
+    private Scheduler () {
+
+    }
+
+    public void schedule (Command command) {
+        pendingCommands.add(command);
+    }
+
+    public void run () {
+        
+        pendingCommands.sort((a,b) -> Integer.compare(a.priority, b.priority));
+
+        priorityMap.clear();
+
+        for (Command command : pendingCommands) {
+            for (Subsystem requirement : command.requirements) {
+                priorityMap.put(requirement, command.priority);
+            }
+        }
+
+        selectionLoop:
+        for (Command command : pendingCommands) {
+            for (Subsystem requirement : command.requirements) {
+                if (command.priority < priorityMap.get(requirement)) {
+                    rejectedCommands.add(command);
+                    continue selectionLoop;
+                }
+            }
+
+            selectedCommands.add(command);
+        }
+
+        pendingCommands.clear();
+
+        for (Command command : selectedCommands) {
+            switch (command.state) {
+                case Command.State.PENDING:
+                    command.start();
+                    pendingCommands.add(command);
+                    break;
+                case Command.State.RUNNING:
+                    command.update();
+                    pendingCommands.add(command);
+                    break;
+                case Command.State.FINISHED:
+                    command.end(false);
+                    break;
+            }
+        }
+
+        for (Command command : rejectedCommands) {
+            switch (command.state) {
+                case Command.State.PENDING:
+                    break;
+                case Command.State.RUNNING:
+                    command.end(true);
+                    break;
+                case Command.State.FINISHED:
+                    command.end(false);
+                    break;
+            }
+        }
+
+        selectedCommands.clear();
+        rejectedCommands.clear();
+    }
+
+    public void clear () {
+        priorityMap.clear();
+        selectedCommands.clear();
+        pendingCommands.clear();
+        rejectedCommands.clear();
+    }
+
+    public static Scheduler getInstance () {
+
+        if (instance == null) {
+            synchronized (Scheduler.class) {
+                if (instance == null) {
+                    instance = new Scheduler();
+                }
+            }
+        }
+
+        return instance;
+    }
     
 }
