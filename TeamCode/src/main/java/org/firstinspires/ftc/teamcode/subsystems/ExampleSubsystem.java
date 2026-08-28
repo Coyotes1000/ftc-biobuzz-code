46d414f6b8b154ebf59e95bc59ed63dc5e590b5c
Alex <alexrichardpoirier@gmail.com>
2026-08-27 20:13:56 -0400

    Creates ExampleSubsystem class for reference.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ExampleSubsystem.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.firstinspires.ftc.teamcode.framework.Subsystem;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ExampleSubsystem extends Subsystem {
+
+    public ExampleSubsystem (HardwareMap hardwareMap) {
+        super(hardwareMap);
+        // Initialise motors, sensors and other hardware.
+    }
+
+    @Override
+    public void update () {
+        // Telemetry and other code that runs each loop.
+    }
+
+    // Other subsystem specific methods.
+
+}
