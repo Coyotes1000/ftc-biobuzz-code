c8bde07113c418b3e8334adb307d702b5c80b1c3
Alex <alexrichardpoirier@gmail.com>
2026-08-27 23:15:08 -0400

    Creates ExampleTeleOp class for reference.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ExampleTeleOp.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import org.firstinspires.ftc.teamcode.framework.BaseOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Example TeleOp", group = "Examples")
+@Disabled
+public class ExampleTeleOp extends BaseOpMode {
+
+    public ExampleTeleOp () {}
+
+    @Override
+    public void gameUpdate () {
+        // Schedule commands based on inputs.
+    }
+
+}
