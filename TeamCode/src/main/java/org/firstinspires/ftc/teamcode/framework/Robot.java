dcc5165d94b65a360d5cd004d6e2f10365f24b6a
Alex <alexrichardpoirier@gmail.com>
2026-09-02 15:21:48 -0400

    Adds scheduleCommand method to Robot class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Robot.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.framework;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.commands.Command;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -11,6 +12,10 @@ public final class Robot {
     public Robot(HardwareMap hardwareMap) {}
 
     public void updateSubsystems() {}
+
+    public void scheduleCommand(Command command) {
+        scheduler.schedule(command);
+    }
 
     public void updateCommands() {
         scheduler.run();
